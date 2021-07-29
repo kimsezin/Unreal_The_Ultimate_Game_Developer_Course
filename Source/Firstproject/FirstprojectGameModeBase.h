5831959c266e486358cdc1e069b6eea4503c0209
kimsezin <48678553+kimsezin@users.noreply.github.com>
2021-07-29 15:29:19 +0900

    Creating BluePrint

---
+++ Source/Firstproject/FirstprojectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstprojectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPROJECT_API AFirstprojectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
