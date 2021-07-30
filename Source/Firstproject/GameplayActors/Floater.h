307520f4df671c362beb53226bd8cac6def25601
kimsezin <48678553+kimsezin@users.noreply.github.com>
2021-07-30 15:28:13 +0900

    16. FVector, UPROPERTY Macro
    함수들이나 언리얼엔진과 c++클래스간의 연결관계등을 더 이해했다...!

--- Source/Firstproject/GameplayActors/Floater.h
+++ Source/Firstproject/GameplayActors/Floater.h
@@ -26,8 +26,18 @@ public:
 	UPROPERTY(VisibleInstanceOnly,BlueprintReadWrite, Category = "Floater Variables")
 	FVector PlacedLocation;
 
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Floater Variables")
+	FVector WorldOrigin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Floater Variables")
+	FVector InitialDirection;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Floater Variables")
+	bool bShouldFloat;
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Floater Variables")
 	bool bInitializeFloaterLocations;
+	
 
 protected:
 	// Called when the game starts or when spawned
