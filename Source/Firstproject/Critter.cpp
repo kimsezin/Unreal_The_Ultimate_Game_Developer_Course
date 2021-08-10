dc3c81dd3b7ed9fb5075b3e03754ef7c2c291793
kimsezin <48678553+kimsezin@users.noreply.github.com>
2021-08-10 18:35:20 +0900

    pawn class

---
+++ Source/Firstproject/Critter.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Critter.h"
+#include "Components/StaticMeshComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+ACritter::ACritter()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent")); // 이 pawn이 쓰는 컴포넌트를 가리키는 것 같음
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent")); // 루트에 붙여줄 Meshcomponent 제작
+	MeshComponent->SetupAttachment(GetRootComponent()); // 붙여줌
+
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera")); // 카메라만들어 root에 붙임
+	Camera->SetupAttachment(GetRootComponent());
+	Camera->SetRelativeLocation(FVector(-300.f, 0.f, 300.f);
+
+}
+
+// Called when the game starts or when spawned
+void ACritter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACritter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACritter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
