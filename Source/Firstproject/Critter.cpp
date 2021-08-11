c3a1e10450cad9a91cdfe02f650f4d2db00721e2
kimsezin <48678553+kimsezin@users.noreply.github.com>
2021-08-11 16:47:27 +0900

    Pawn Movement (Collision)

--- Source/Firstproject/Critter.cpp
+++ Source/Firstproject/Critter.cpp
@@ -4,6 +4,7 @@
 #include "Critter.h"
 #include "Components/StaticMeshComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
 
 // Sets default values
 ACritter::ACritter()
@@ -21,7 +22,11 @@ ACritter::ACritter()
 	Camera->SetRelativeLocation(FVector(-300.f, 0.f, 300.f));
 	Camera->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
 
-	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	AutoPossessPlayer = EAutoReceiveInput::Player1; //게임 시점 바꾸기
+
+	CurrentVelocity = FVector(0.f);
+
+	MaxSpeed = 100.f;
 }
 
 // Called when the game starts or when spawned
@@ -35,7 +40,9 @@ void ACritter::BeginPlay()
 void ACritter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	
+	FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+	SetActorLocation(NewLocation);
 }
 
 // Called to bind functionality to input
@@ -43,5 +50,17 @@ void ACritter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ACritter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ACritter::MoveRight);
 }
 
+void ACritter::MoveForward(float Value)
+{
+	CurrentVelocity.X = FMath::Clamp(Value, -1.f, 1.0f) * MaxSpeed; //1~-1사이의 값으로 비율에 맞게 설정
+
+}
+
+void ACritter::MoveRight(float Value)
+{
+	CurrentVelocity.Y = FMath::Clamp(Value, -1.f, 1.0f) * MaxSpeed;
+}
