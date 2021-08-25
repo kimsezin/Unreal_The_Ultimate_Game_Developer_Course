4b9852f2c03bb60f5b68af4c991b5c451051b8d7
kimsezin <48678553+kimsezin@users.noreply.github.com>
2021-08-25 19:02:25 +0900

    Character

---
+++ Source/Firstproject/Main.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Main.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+AMain::AMain()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 카메라 붐 (스프링암, 플레이어에 충돌이 있으면 당긴다.)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 600.0f; // 카메라와 플레이어간 거리
+	CameraBoom->bUsePawnControlRotation = true; // 컨트롤러 기준으로 회전
+
+	// 따라다니는 카메라 생성
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); //원래 스프링암에는 소켓이 가장 끝에 붙어있음, 스프링암에 카메라 부착
+	// let the boom adjust to match the controller orientation
+	FollowCamera->bUsePawnControlRotation = false; // 카메라는 고정
+	
+	// Set our turn rate for input
+	BaseTurnRate = 65.f;
+	BaseLookUpRate = 65.f;
+
+}
+
+// Called when the game starts or when spawned
+void AMain::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMain::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMain::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AMain::MoveForward(float value)
+{
+	if ((Controller != nullptr) && (value != 0.0f))
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	}
+}
+
+void AMain::MoveRight(float value)
+{
+
+}
+
---
+++ Source/Firstproject/Main.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Main.generated.h"
+
+UCLASS()
+class FIRSTPROJECT_API AMain : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMain();
+
+	/** Camera boom를 플레이어 뒤에 위치 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true")) //블루프린트 내부에서 액세스 가능, 외부에서 액세스 불가
+	class USpringArmComponent* CameraBoom;
+	
+	/** 따라다니는 카메라 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+	/** Base turn rates to scale turning functions for the camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseTurnRate;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseLookUpRate;
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	/** called for forwards//backwards input */
+	void MoveForward(float value);
+
+	/** called for side to side input */
+	void MoveRight(float value);
+};
